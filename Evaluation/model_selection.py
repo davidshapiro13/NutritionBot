56f3bb6639142d01988bc0e1210d0969a3d79ed7
davidshapiro13 <davidshapiro@brandeis.edu>
2026-03-16 23:54:56 -0400

    model selection work

--- Evaluation/model_selection.py
+++ Evaluation/model_selection.py
@@ -1,35 +1,125 @@
 import requests
 from llmproxy import LLMProxy
-from prompts import queries
+from prompts import open_response, multi_choice
 import random
 
-client = LLMProxy()
-models = ['4o-mini', 'us.anthropic.claude-3-haiku-20240307-v1:0', 'azure-phi3', 'us.meta.llama3-2-3b-instruct-v1:0']
+class Model():
+    def __init__(self, name):
+        self.name = name
+        self.multi_correct = 0
+        self.best_open = 0
+        self.answers = []
 
-system_instructions = (
-            "You are an expert nutritionist that specializes in developing regions.")
+    def increase_correct_multi(self):
+        self.multi_correct += 1
+    def increase_best_open(self):
+        self.best_open += 1
+    def generate_answers(self):
+        for question in open_response:
+            session_id_value = "convo" + str(random.random())
+            response = client.generate(
+                        model = self.name,
+                        system = open_response_instructions,
+                        query = question,
+                        temperature = temperature_value,
+                        lastk = last_queries,
+                        session_id = session_id_value,
+                        rag_usage = False)["result"]
+            
+            self.answers.append(response)
+
+    def __repr__(self):  
+        return "Model: " + self.name + "| Multiple Choice: " + str(self.multi_correct) + "\n"
+
+client = LLMProxy()
+models = [Model('4o-mini'), Model('us.anthropic.claude-3-haiku-20240307-v1:0'), Model('azure-phi3'), Model('us.meta.llama3-2-3b-instruct-v1:0')]
+judge_models = [Model('4o-mini'), Model('us.anthropic.claude-3-haiku-20240307-v1:0'), Model('azure-phi3'), Model('us.meta.llama3-2-3b-instruct-v1:0')]
+
+multiple_choice_instructions = (
+            "You are an expert nutritionist located in Boston, MA. This is a multiple choice question. Only answer with a, b, c, d, e, f (depending on number of options in question)")
+
+open_response_instructions = (
+            "You are an expert nutritionist located in Boston, MA. This is an open response question. Write a friendly, informational, accurate answer to the following question."
+)
+judge_instructions = (
+    """You are an expert nutritionist judging answers to helath questions.
+    Answer that you like 1 or 2 better where 1 is the first option and 2 is the second option.
+    Base your answer on the accuracy of the response, the clarity, the completeness and the tone (we want a kind, friendly tone)"""
+)
     
 temperature_value = 0.0
 last_queries = 2
 rag_enabled = True
-    
-with open("model_eval_results.txt", "w") as file:
-    for question in queries:
-        file.write("QUESTION: " + question + "\n\n")
-        print(question)
 
-        for model_name in models:
-            print(model_name)
+def multiple_choice_section():
+    for question in multi_choice:
+        for model in models:
             session_id_value = "convo" + str(random.random())
-            file.write("MODEL: " + model_name + "\n")
-
             response = client.generate(
-                model = model_name,
-                system = system_instructions,
-                query = question,
-                temperature = temperature_value,
-                lastk = last_queries,
-                session_id = session_id_value,
-                rag_usage = False)
+                        model = model.name,
+                        system = multiple_choice_instructions,
+                        query = question["question"],
+                        temperature = temperature_value,
+                        lastk = last_queries,
+                        session_id = session_id_value,
+                        rag_usage = False)
+            if question["answer"] == response["result"]:
+                model.increase_correct_multi()
+    print(models)
+
+def write_to_file(models):
+    with open("model_eval_results.txt", "w") as file:
+        for i, question in enumerate(open_response):
+            file.write("QUESTION: " + question + "\n\n")
+            for model in models:
+                file.write("MODEL: " + model.name + "\n")
+                file.write(model.answers[i] + "\n")
+
+def LLM_as_Jury(models):
+    for i, question in enumerate(open_response):
+        for judge in judge_models:
+            response1 = models[0].answers[i]
+            response2 = models[1].answers[i]
+            response3 = models[2].answers[i]
+            response4 = models[3].answers[i]
+            round1_winner, round1_res = judge_vote(judge.name, models[0], models[1], response1, response2)
+            round2_winner, round2_res = judge_vote(judge.name, models[0], models[1], response3, response4)
+            round3_winner, _ = judge_vote(judge.name, round1_winner, round2_winner, round1_res, round2_res)
+            round3_winner.increase_best_open()
+    best_model = None
+    top_score = 0
+    for model in models:
+        if model.best_open > top_score:
+            top_score = model.best_open
+            best_model = model
+    return best_model, top_score
+
             
-            file.write(response['result'] + "\n") 
+
+def judge_vote(judge_model, model1, model2, ans1, ans2):
+    session_id_value = "convo" + str(random.random())
+    response = client.generate(
+        model = judge_model,
+        system = judge_instructions,
+        query = "Option 1: " + ans1 + "\n\n | Option 2: " + ans2,
+        temperature = temperature_value,
+        lastk = last_queries,
+        session_id = session_id_value,
+        rag_usage = False)["result"]
+    if response == '1':
+        return model1, ans1
+    if response == '2':
+        return model2, ans2
+    return None
+
+                
+
+def open_response_section():
+    for model in models:
+        model.generate_answers()
+    write_to_file(models)
+    best_model, top_score = LLM_as_Jury(models)
+    print("Best Model: ", best_model, " | Score: ", top_score)
+
+multiple_choice_section()
+open_response_section()
--- Evaluation/prompts.py
+++ Evaluation/prompts.py
@@ -1,21 +1,75 @@
-model_eval_prompt1 = """
-    I live in Sudan. What is a cheap and healthy meal I can make for dinner? Why is it healthy?
+model_eval_open_response1 = """
+    I live in Somerville, MA. What is a cheap and healthy meal I can make for dinner? Why is it healthy?
 """
 
-model_eval_prompt2 = """
-    I live in Sudan. I am going to eat rice, beans and beer for dinner. What else should I consider eating? I have a gluten allergy.
+model_eval_open_response2 = """
+    I am going to eat rice, beans and beer for dinner. What else should I consider eating? I have a gluten allergy.
 """
 
-model_eval_prompt3 = """
+model_eval_open_response3 = """
     My stomach hurts a lot right now. What should I do to treat it?
 """
 
-model_eval_prompt4 = """
-    I was going to feed my 10 month old some honey. That should be fine because it is a liquid, right?
+model_eval_open_response4 = """
+    Yesterday I ate toast and jam for breakfast, cobb salad for lunch and sushi for dinner. What types of foods should I focus on today?
 """
 
-model_eval_prompt5 = """
-    What are the components of a healthy well-rounded meal?
+model_eval_open_response5 = """
+    I want to make a well-rounded meal for my family with $20. What is a broken down budget for it?
 """
 
-queries = [model_eval_prompt1, model_eval_prompt2, model_eval_prompt3, model_eval_prompt4, model_eval_prompt5]
+model_eval_multi_choice1 = {"question": """
+    Today is Wednesday the 10th. I have some rice in my fridge from last Thursday. What should I do with it?
+    a) Eat it so you do not waste it.
+    b) Throw it away. It has been too long and can grow bacteria.
+    c) Smell it. If it smells fine, it is fine.
+    d) Mix it into another meal
+    e) None of the above
+""", "answer": "b"}
+
+model_eval_multi_choice2 = {"question": """
+    What are common symptoms of food poisoning?
+    a) Nausea
+    b) Loss of Taste and Smell
+    c) Stomach Cramps
+    d) Sore throat
+    e) a and c
+    f) c and d
+""", "answer": "e"}
+
+model_eval_multi_choice3 = {"question": """
+    Which is not a food group?
+
+    a) Fruits
+    b) Grains
+    c) Starches
+    d) Dairy
+    e) Vegetables
+""", "answer": "c"}
+
+model_eval_multi_choice4 = {"question": """
+    Which of the following is not safe to feed a nine-month-old?
+    a) chicken
+    b) honey
+    c) banana
+    d) rice
+    e) all of these are safe
+    f) none of these are safe
+""", "answer": "b"}
+
+model_eval_multi_choice5 = {"question": """
+    I have an allergy to gelatin. What foods should I check the ingredients of?
+
+    a) Tomato Sauce
+    b) Chocolate Mousse
+    c) Tofu
+    d) Marshmellows
+    e) a and b
+    f) b and d
+""", "answer": "f"}
+
+
+
+
+open_response = [model_eval_open_response1, model_eval_open_response2, model_eval_open_response3, model_eval_open_response4, model_eval_open_response5]
+multi_choice = [model_eval_multi_choice1, model_eval_multi_choice2, model_eval_multi_choice3, model_eval_multi_choice4, model_eval_multi_choice5]
